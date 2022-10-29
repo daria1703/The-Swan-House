30f74090585a24d0f8b4ec4a87d2d67016a74a7e
Daria <daria.citak1703@gmail.com>
2022-10-29 17:35:48 +0200

    Router6

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './Pages/Home';
+import Shop from './Pages/Shop';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import ErrorPage from './Pages/ErrorPage';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/shop">Shop</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/shop" element={<Shop/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="*" element={<ErrorPage/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+// import Button from "react-bootstrap/Button"
+
+function Home() {
+  return (
+    <div>
+      Home
+      {/* <Button className="btn btn-secondary">This is my button</Button> */}
+    </div>
+  )
+}
+
+export default Home
