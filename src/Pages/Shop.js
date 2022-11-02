5c2c6de0baa785f107d4669f17a6d114186e8a32
Daria <daria.citak1703@gmail.com>
2022-11-02 19:17:26 +0100

    Connection

--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -1,46 +1,72 @@
 import React from 'react'
 import {useEffect, useState} from "react";
 import Product from '../Components/Product';
+import { Link } from 'react-router-dom';
+class Shop extends React.Component {
 
-function Shop() {
-    const [products, setProducts] = useState([]);
+    constructor(props) {
   
-
-  useEffect(() =>{
-    fetchProducts()
-  }, []);
-
-  const fetchProducts = async () =>{
-    let url = 'https://gnikdroy.pythonanywhere.com/api/book/';
-
-    while (url) {
-      console.log(url);
-      let data = await fetch(url);
-      let products = await data.json();
-      setProducts(products.results);
-      url = products.next;
-      console.log(products);
+      super(props);
+  
+      this.state = {
+        items: [],
+        isLoaded: false
+      }
+  
     }
+  
+    /**
+     * componentDidMount
+     *
+     * Fetch json array of objects from given url and update state.
+     */
+    componentDidMount() {
+  
+      fetch('https://swan-server.herokuapp.com/products')
+        .then(res => res.json())
+        .then(json => {
+          this.setState({
+            items: json,
+            isLoaded: true,
+          })
+        }).catch((err) => {
+          console.log(err);
+        });
+  
+    }
+  
+    render() {
+  
+      const { isLoaded, items } = this.state;
+  
+      if (!isLoaded)
+        return <div>Loading...</div>;
+  
+        // clik()
+  
+      return (
+        <div className="content">
 
-    console.log(products);
-      
-
-    // const data = await fetch('https://gnikdroy.pythonanywhere.com/api/book/');
-    // const books = await data.json();
-    // console.log(books)
-    // setPopular(books.results);
-    // setFiltered(books.results)
-  };
-  return (
-    <div className="App">
-      <div class="popular-books">
-        {products.map(product =>{
-          return <Product key={product.id} product={product}/>;
-        })}
-      </div>
-    </div>
-  );
-
-}
-
-export default Shop
+            {items.map(item => (
+              <section key={item.id}>
+                <p>{item.title}</p>
+                <img src={item.img}/>
+                {/* <div class="image">
+                  <img class="image__img" src={item.image} alt={item.title} />
+                  <div class="image__overlay image__overlay--primary">
+                    
+                    <div className="btn-div"> 
+                             <button type="submit" className="button3">
+                                 <Link class="link-btn2" to={`/film/${item.id}` }>Check!</Link></button>
+                         </div>
+                         <div class="image__title">{item.title}</div>
+                  </div>
+                </div> */}
+              </section>
+            ))}
+        </div>
+      );
+    }
+  }
+  
+  export default Shop;
