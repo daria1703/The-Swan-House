4649b4060cc223d8445450a8ed1087f111b3865e
Daria <daria.citak1703@gmail.com>
2023-02-06 10:32:40 +0100

    Update

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -2,43 +2,77 @@ import React from "react";
 import "../css/login.css";
 import logo from '../img/logo.png';
 import bg from '../img/login.png'
-import { useState } from "react";
-import { login } from "../redux/apiCalls";
-import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
-const Login = () => {
+export default class Login extends React.Component {
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const dispatch = useDispatch();
-    const { isFetching, error } = useSelector((state) => state.user);
+	constructor(props) {
+		super(props)
 
-    const handleClick = (e) => {
-        e.preventDefault();
-        login(dispatch, { email, password });
-    };
-	
+		this.state = {
+			email: " ",
+			password: " ",
+            isLogged: false
+		}
+        this.handleSubmit = this.handleSubmit.bind(this)
+	}
+
+    // handleChange = e => {
+	// 	this.setState({ [e.target.name]: e.target.value })
+	// }
+
+    handleSubmit = e => {
+		e.preventDefault()
+		// console.log(this.state)
+		// axios.post("http://localhost:3000/users/login/", this.state)
+        // .then(res => alert(res.data.message))
+        const {email, password} = this.state;
+        console.log(email, password);
+        fetch("http://localhost:3000/users/login/", {
+            method: "POST",
+            crossDomain: true,
+            headers:{
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                "Access-Control-Allow-Origin": "*"
+            },
+            body: JSON.stringify({
+                email,
+                password
+            }),
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data, "login");
+            if (data.status === "ok"){
+                alert("Login successful");
+                window.localStorage.setItem("token", data.data);
+                window.localStorage.setItem("loggedIn", true);
+                // window.localStorage.setItem("token", data.data);
+                window.location.href="./";
+            }
+        })}
+
+    render() {
         return(
             <div className="login">
                 <img src={bg} alt="background"/>
-                <form className="login_form">
+                <form onSubmit={this.handleSubmit} className="login_form">
                 <div className="logo_container">
 						<p>The swan house</p>
 						<img src={logo} alt="logo" className="logo_register"/>
 						<p>The swan house</p>
 					</div>
                 <h1>Welcom Back!</h1>
-                <input type="email" name="email" onChange={(e) => setEmail(e.target.value)} placeholder="Enter your e-mail"/>
-                <input type="password" name="password" placeholder='Your Password' onChange={(e) => setPassword(e.target.value)} />
-                <button className="btn btn-primary btn-login" onClick={handleClick} disabled={isFetching}>Login</button>
-                {error && <p className="error">Something went wrong...</p>}
+                <input type="email" name="email" onChange={(e) => this.setState({email: e.target.value})} placeholder="Enter your e-mail"/>
+                <input type="password" name="password" placeholder='Your Password' onChange={(e) => this.setState({password: e.target.value})} />
+                <button  className="btn btn-primary btn-login" onClick={this.handleSubmit}>Login</button>
                 <a href="./resetPassword" className="login-a">Forgot password?</a>
                 <p>Haven't got an account? <a href="/personAdd" className="register-a">SIGN UP </a></p>
 
                 </form>
             </div>
         )
+    }
 
 }
-
-export default Login
